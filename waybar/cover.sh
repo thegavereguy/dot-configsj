d13d14639c06df2768310dbd360fa07c4311920e
thegavereguy <dallacia.enrico@protonmail.com>
2023-09-06 11:30:35 +0200

    Updated Hyprland and Waybar
    Added Cava config

--- waybar/cover.sh
+++ waybar/cover.sh
@@ -4,6 +4,13 @@ if [[ $album_art ]]
 then
 	curl -s  "${album_art}" --output "/tmp/cover.jpeg"
 	echo "/tmp/cover.jpeg"
+fi
+
+album_art=$(playerctl -p strawberry metadata mpris:artUrl)
+if [[ $album_art ]]
+then
+	curl -s  "${album_art}" --output "/tmp/cover.jpg"
+	echo "/tmp/cover.jpg"
 	exit
 fi
 
